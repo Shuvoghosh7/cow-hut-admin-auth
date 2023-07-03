a6a55a9cc52205c31c0198957a32402d583ead91
Shuvoghosh7 <shuvoghosh1610@gmail.com>
2023-07-03 10:34:32 +0600

    update order services

--- src/app/modules/orders/order.services.ts
+++ src/app/modules/orders/order.services.ts
@@ -9,47 +9,52 @@ import { Order } from './order.module';
 
 type CowModel = {
   price: number;
-  seller:Types.ObjectId
-} 
-const createOrder = async (order:IOrder) => {
-
+  seller: Types.ObjectId;
+};
+const createOrder = async (order: IOrder) => {
   const buyer = await User.findById(order.buyer);
   const cow = await Cow.findById<CowModel>(order.cow);
   if (!buyer) {
-    throw new Error("Buyer not found"); 
+    throw new Error('Buyer not found');
   }
   if (!cow) {
-    throw new Error("Cow not found"); 
+    throw new Error('Cow not found');
   }
-  
+
   if (buyer.budget < cow.price) {
     throw new Error('Insufficient funds to buy the cow.');
   }
 
   const session = await mongoose.startSession();
-  session.startTransaction();
-  await Cow.findByIdAndUpdate(order.cow, { label: 'sold out' });
+  try {
+    session.startTransaction();
+    await Cow.findByIdAndUpdate(order.cow, { label: 'sold out' });
 
     // Deduct the cost of the cow from the buyer's budget
     await User.findByIdAndUpdate(order.buyer, {
       budget: buyer.budget - cow.price,
     });
-     // Update the seller's income by adding the cost of the cow
-  const sellerId = cow.seller._id;
-  const cowPrice = cow.price;
-  await User.updateOne({ _id: sellerId }, { $inc: { income: cowPrice } });
+    // Update the seller's income by adding the cost of the cow
+    const sellerId = cow.seller._id;
+    const cowPrice = cow.price;
+    await User.updateOne({ _id: sellerId }, { $inc: { income: cowPrice } });
+    const result = await Order.create(order);
 
-  const result = await Order.create(order);
+    await session.commitTransaction();
+    await session.endSession();
 
-  await session.commitTransaction();
-  session.endSession();
-  return result;
+    return result;
+  } catch (error) {
+    await session.abortTransaction();
+    await session.endSession();
+    throw error;
+  }
 };
+
 const getAllOrder = async () => {
-  const result = await Order.find().populate("cow").populate("buyer");
+  const result = await Order.find().populate('cow').populate('buyer');
   return result;
 };
-
 
 export const OrderService = {
   createOrder,
